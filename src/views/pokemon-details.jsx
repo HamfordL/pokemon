8e3b2b360003114548e6926201281b14bd921702
Hamford <hamps_789@hotmail.com>
2022-11-09 23:13:20 -0500

    added API information on set pokemons type, added front and back img of sprite

--- src/views/pokemon-details.jsx
+++ src/views/pokemon-details.jsx
@@ -1,9 +1,46 @@
+import { useEffect, useState } from "react";
+import { Spin } from "antd";
+
 import { useParams } from "react-router-dom";
 
 const PokemonDetails = () => {
   const urlParams = useParams();
+  const [pokemon, setPokemon] = useState(null);
 
-  return <div>Learn more details about pokemon #{urlParams.id}</div>;
+  useEffect(() => {
+    if (!pokemon) {
+      fetch("https://pokeapi.co/api/v2/pokemon/" + urlParams.id)
+        .then((response) => response.json())
+        .then((data) => setPokemon(data));
+    }
+  });
+  if (!pokemon) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Spin size="large" tip="Loading pokemon data" />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <img src={pokemon.sprites.back_default} alt={pokemon.name} />
+      <br />
+      name: {pokemon.name}
+      <br />
+      type: {pokemon.types.map((typeObject) => typeObject.type.name).join(", ")}
+      <br />
+    </div>
+  );
 };
 
 export default PokemonDetails;
