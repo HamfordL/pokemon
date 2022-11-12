b6ccf91e07d6eb7d8575470b8da2d8804a2f357c
Hamford <hamps_789@hotmail.com>
2022-11-12 17:21:13 -0500

    added fetch paths, but dropped the response

--- src/views/pokemon-details.jsx
+++ src/views/pokemon-details.jsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from "react";
+
 import { Spin } from "antd";
 
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const PokemonDetails = () => {
   const urlParams = useParams();
   const [pokemon, setPokemon] = useState(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!pokemon) {
       fetch("https://pokeapi.co/api/v2/pokemon/" + urlParams.id)
         .then((response) => response.json())
         .then((data) => setPokemon(data));
+
+      Promise.all();
+      fetch()
+        .then((res) => res.json())
+        .then((res) =>
+          fetch("https://pokeapi.co/api/v2/pokemon-species/" + urlParams.id)
+            .then((res) => res.json())
+
+            .then((species_res) =>
+              fetch("https://pokeapi.co/api/v2/evolution-chain/" + urlParams.id)
+            )
+            .then((res) => res.json())
+            .then((data) => setPokemon(data))
+        );
     }
   });
+
   if (!pokemon) {
     return (
       <div
@@ -33,8 +51,9 @@ const PokemonDetails = () => {
 
   return (
     <div>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <img src={pokemon.sprites.back_default} alt={pokemon.name} />
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />{" "}
+      <img src={pokemon.sprites.back_default} alt={pokemon.name} />{" "}
+      <button onClick={() => navigate(-1)}>Go back</button>
       <br />
       name: {pokemon.name}
       <br />
@@ -47,6 +66,7 @@ const PokemonDetails = () => {
       <br />
       <br />
       evolution chain:{" "}
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
     </div>
   );
 };
